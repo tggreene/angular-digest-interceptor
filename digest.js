e11114e1255cc90a7fc6beeaf2a5617b5871c806
Alexey Ozerov <alexozerov@gmail.com>
2016-03-15 15:46:21 +0300

    Fixed brackets

--- digest.js
+++ digest.js
@@ -149,6 +149,7 @@ function DigestAuthInterceptor(initialUsername, initialPassword, maximumRetries,
           config: config
         }
       );
+    });
 
     return deferredResponse.promise;
   }
